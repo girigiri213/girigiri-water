64904c50adbd18518b0152990a2726344db649b1
Sun Peng <sunpeng1995@gmail.com>
2016-06-28 13:31:34 +0800

    add a container in login page

--- dist/js/components/LoginForm.js
+++ dist/js/components/LoginForm.js
@@ -1,31 +1,33 @@
 import React from 'react'
 const LoginForm = ({ onSubmit }) => (
-  <div className="row">
-      <div className="col-md-4 col-md-offset-4">
-          <div className="login-panel panel panel-default">
-              <div className="panel-heading">
-                  <h3 className="panel-title">Please Sign In</h3>
-              </div>
-              <div className="panel-body">
-                  <form role="form">
-                      <fieldset>
-                          <div className="form-group">
-                              <input className="form-control" placeholder="E-mail" name="email" type="email" autofocus />
-                          </div>
-                          <div className="form-group">
-                              <input className="form-control" placeholder="Password" name="password" type="password" value="" />
-                          </div>
-                          <div className="checkbox">
-                              <label>
-                                  <input name="remember" type="checkbox" value="Remember Me" />Remember Me
-                              </label>
-                          </div>
-                          <a href="#" className="btn btn-lg btn-success btn-block">Login</a>
-                      </fieldset>
-                  </form>
-              </div>
-          </div>
-      </div>
+  <div className="container">
+    <div className="row">
+        <div className="col-md-4 col-md-offset-4">
+            <div className="login-panel panel panel-default">
+                <div className="panel-heading">
+                    <h3 className="panel-title">Please Sign In</h3>
+                </div>
+                <div className="panel-body">
+                    <form role="form">
+                        <fieldset>
+                            <div className="form-group">
+                                <input className="form-control" placeholder="E-mail" name="email" type="email" autofocus />
+                            </div>
+                            <div className="form-group">
+                                <input className="form-control" placeholder="Password" name="password" type="password" value="" />
+                            </div>
+                            <div className="checkbox">
+                                <label>
+                                    <input name="remember" type="checkbox" value="Remember Me" />Remember Me
+                                </label>
+                            </div>
+                            <a href="#" className="btn btn-lg btn-success btn-block">Login</a>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
   </div>
 )
 
