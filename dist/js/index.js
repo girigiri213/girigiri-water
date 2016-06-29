28830001b99dbd0e19139842fef2b514f3c806d6
Ziyi Yan <cxfyzy@gmail.com>
2016-06-29 10:27:22 +0800

    Added new dependencies

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,11 @@
+import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 import App from './components/App'
+import configureStore from './store/configureStore'
 
-let store = createStore(store => store)
+const store = configureStore()
 
 render(
   <Provider store={store}>
